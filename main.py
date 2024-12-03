2e0dbd62ddf0c33df42a4af595a7ca916dc19a3a
anabernal <anabernal297@gmail.com>
2024-12-02 23:00:51 -0300

    Agregacion de metodos
    Se agregan algunos metodos mas de prueba

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_user():
+    return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts():
+    return {"message": "successfully created posts"}
