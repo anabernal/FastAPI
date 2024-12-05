ce53827c9f1d2e864ceda029ff17ca60d3bdb0af
anabernal <anabernal297@gmail.com>
2024-12-05 12:11:40 -0300

    se agrega la funcion para buscar un solo post

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
     publised: bool =True
+   
 
+
+
+
+my_posts=[{"title":"title of post 1", "content": "content of post 1","id": 1}, {"title":"favorite foods", "content":"sushi, asado", "id":2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"]==id:
+            return p
+    
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-
 
 @app.get("/")
 def get_user():
@@ -21,11 +30,17 @@ def get_user():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 @app.post("/posts")
 def create_posts(post:Post):
-    print(post)
-    print(post.dict())
-    return{"data": "new post"}
+    post_dict=post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return{"data":post_dict}
     
+@app.get("/posts/{id}")
+def get_post(id:int):
+  post=find_post(id)
+  print(id)
+  return {"post_detail":post}
