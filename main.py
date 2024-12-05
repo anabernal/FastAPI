95a29b8e5b1ef058e030f9f2dfaf3a37e7252e81
anabernal <anabernal297@gmail.com>
2024-12-05 20:58:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ class Post(BaseModel):
     title: str
     content: str
     publised: bool =True
+   
 
+
+
+
+my_posts=[{"title":"title of post 1", "content": "content of post 1","id": 1}, {"title":"favorite foods", "content":"sushi, asado", "id":2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"]==id:
+            return p
+    
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -23,9 +34,10 @@ def get_posts():
 
 @app.post("/posts")
 def create_posts(post:Post):
-    print(post)
-    print(post.dict())
-    return{"data": "new post"}
+    post_dict=post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return{"data":post_dict}
     
 @app.get("/posts/{id}")
 def get_post(id:int):
