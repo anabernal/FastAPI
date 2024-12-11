ed44b0c2bd2457228d00892bce589f35db9a5a97
anabernal <anabernal297@gmail.com>
2024-12-11 11:42:09 -0300

    ordenamiento de schemas

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 
 from fastapi import FastAPI, Response,status,HTTPException, Depends
 from fastapi.params import Body
-from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from .import models 
+from .import models, schemas 
 from sqlalchemy.orm import Session 
 from .database import engine, get_db
 
@@ -16,10 +15,6 @@ app = FastAPI()
 
 
     
-class Post(BaseModel):
-    title:str
-    content:str
-    published: bool=True
 
 while True:
     try:
@@ -47,19 +42,17 @@ def find_index_post(id):
             return id
 
 
-@app.get("/sqlalchemy")
+
+@app.get("/posts")
 def test_post(db:Session=Depends(get_db)):
+    #cursor.execute("""SELECT * FROM posts""")
+    #posts=cursor.fetchall()
     posts=db.query(models.Post).all()
     return{"data": posts}
 
-@app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts=cursor.fetchall()
-    return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post:Post, db:Session=Depends(get_db)):
+def create_posts(post:schemas.PostCreate, db:Session=Depends(get_db)):
     #cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s,%s,%s) RETURNING *""" ,
     #               (post.title, post.content, post.publised))
     #new_post=cursor.fetchone()
@@ -98,7 +91,7 @@ def delete_post(id:int,db: Session=Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id:int, updated_post:Post, db:Session=Depends(get_db)):
+def update_post(id:int, updated_post:schemas.PostCreate, db:Session=Depends(get_db)):
     #cursor.execute("""UPDATE posts SET title= %s, content=%s, published=%s 
     #               WHERE id=%s RETURNING * """,(post.title,post.content,post.publised,str(id)))
     #update_post=cursor.fetchone()
