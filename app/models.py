0bf97314a1675e8fcc6d49820be1310cad90a01a
anabernal <anabernal297@gmail.com>
2024-12-11 14:34:58 -0300

    Se crea un modelo de vpn

--- app/models.py
+++ app/models.py
@@ -9,3 +9,22 @@ class Post(Base):
     content=Column(String,nullable=False)
     published=Column(Boolean, server_default='True', nullable=False)
     created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
+
+class vpn(Base):
+    __tablename__="vpns"
+    clienteExterno=Column(String,nullable=False)
+    marcaEquipoRoshka=Column(String,nullable=False)
+    versionEquipoRoshka=Column(String,nullable=False)
+    marcaEquipoCliente=Column(String,nullable=False)
+    versionEquipoCliente=Column(String,nullable=False)
+    claveCompartida=Column(String,nullable=False)
+    esquemaDeEncriptacion=Column(String,nullable=False)
+    grupoDH=Column(String,nullable=False)
+    mainOAggressive=Column(String,nullable=False)
+    lifetime=Column(Integer,nullable=False)
+    encapsulacion=Column(String,nullable=False)
+    algoritmoEncriptacion=Column(String,nullable=False)
+    pfs=Column(Boolean, nullable=False)
+    lifetime=Column(Integer,nullable=False)
+
+    
