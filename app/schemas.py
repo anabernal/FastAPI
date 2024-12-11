f0ce57d8681454862b1ebbfc51c952cdd7c299f9
anabernal <anabernal297@gmail.com>
2024-12-11 15:08:50 -0300

    Modificacion de schema.py

--- app/schemas.py
+++ app/schemas.py
@@ -8,4 +8,38 @@ class PostCreate(PostBase):
     pass
 
 
+class VpnBase(BaseModel):
+    direccionIPRoshka:str
+    direccionIPCliente:str
+    clienteExterno:str
+    marcaEquipoRoshka:str
+    versionEquipoRoshka:str
+    marcaEquipoCliente:str
+    versionEquipoCliente:str
+    claveCompartida:str
+    esquemaDeEncriptacion:str
+    grupoDH:str
+    mainOAggressive:str
+    lifetime:int
+    encapsulacion:bool=True
+    algoritmoEncriptacion:str
+    pfs=bool
+    lifetime:str
+    dominioEncriptacionRoshka:str
+    dominoEncriptacionCliente:str
+
+class VpnCreate(PostBase):
+    pass
+
+class VpnUpdateRoshka(PostBase):
+    pass
+
+class VpnUpdateCliente(PostBase):
+    clienteExterno:str
+    direccionIPCliente:str
+    marcaEquipoCliente:str
+    versionEquipoCliente:str
+    claveCompartida:str
+    dominoEncriptacionCliente:str
+
 
